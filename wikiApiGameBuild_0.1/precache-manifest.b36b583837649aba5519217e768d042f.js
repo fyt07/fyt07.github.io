0cc2996f996791bcaf5233faa43af617a6e4bc50
Alexandr <fyt007@mail.ru>
2018-12-30 11:54:24 +0300

    wikwApi

---
+++ wikiApiGameBuild_0.1/precache-manifest.b36b583837649aba5519217e768d042f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d4bab5d20712ff089045",
+    "url": "/static/js/main.d4bab5d2.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "d4bab5d20712ff089045",
+    "url": "/static/css/main.e41c2174.chunk.css"
+  },
+  {
+    "revision": "fbd4ee65ccc35a6b0ebc0cc5e1cb8145",
+    "url": "/index.html"
+  }
+];
